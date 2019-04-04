73622927308b2cac029b3d676a05b7512f9350db
정효진 <jn2901@naver.com>
2019-04-04 15:39:47 +0900

    DAO VO

---
+++ ServletStudy/src/com/vigteam/study/servlet/BaseServlet.java
@@ -0,0 +1,25 @@
+package com.vigteam.study.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public abstract class BaseServlet extends HttpServlet {
+	protected String dbuser = null;
+	protected String dbpass = null;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config); // 절대 지우지 맙시다.
+		
+		ServletContext context = getServletContext();//web.xml의 전체 context 불러오는 것
+		dbuser = context.getInitParameter("dbuser");
+		dbpass = context.getInitParameter("dbpass");
+		
+		System.out.println("dbuser:" + dbuser);
+		System.out.println("dbpass:" + dbpass);
+		
+	}
+
+}
